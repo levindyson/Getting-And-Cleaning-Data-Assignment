5ed288bd93704d7b1640419482432dca8848b7df
Levin Dyson <40443340+levindyson@users.noreply.github.com>
2019-09-20 19:02:26 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-# The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set.
+# The purpose of this project is to demonstrate my ability to collect, work with, and clean a data set.
 # The script should do the following:
 
 # 1. Merges the training and the test sets to create one data set.
